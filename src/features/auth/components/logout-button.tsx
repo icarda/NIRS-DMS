4c245b47565793ae9f62ea65ae903f345a834419
Elab4d1 <hassanflv@gmail.com>
2025-05-09 18:14:03 +0000

    fix never ending navigation progress when user sign out within same page

--- src/features/auth/components/logout-button.tsx
+++ src/features/auth/components/logout-button.tsx
@@ -1,14 +1,24 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+import NProgress from "nprogress";
+
 import { logout } from "@/features/auth/actions/logout";
 
-interface LogoutButtonProps {
-  children?: React.ReactNode;
-}
+export const LogoutButton = ({ children }: { children: React.ReactNode }) => {
+  const router = useRouter();
 
-export const LogoutButton = ({ children }: LogoutButtonProps) => {
   const onClick = async () => {
-    logout();
+    NProgress.remove();
+    NProgress.start();
+
+    try {
+      await logout();
+      router.refresh();
+      setTimeout(() => NProgress.done(), 300);
+    } catch (e) {
+      NProgress.done();
+    }
   };
 
   return <span onClick={onClick}>{children}</span>;
